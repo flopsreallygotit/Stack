2fdb29a97ab1335a1bd389a0aa55bc24f54e3a4e
Egor Shevyahov <flopsreallygotit@gmail.com>
2022-11-01 12:11:19 +0300

    Headers fixed. README added. ssize_t removed. Memory leaks fixed.

--- Example/main.cpp
+++ Example/main.cpp
@@ -5,6 +5,20 @@ int main()
     stack *stk = stackConstructor(10);
 
     stackPush(stk, 1);
+    stackPush(stk, 4);
+    stackPush(stk, 9);
+
+    stackDump(stk);
+
+    elem_t myStackValues[10] = {};
+
+    stackPop(stk, &myStackValues[0]);
+    stackPop(stk, &myStackValues[1]);
+    stackPop(stk, &myStackValues[2]);
+
+    printf("My stack values: %d %d %d;\n", myStackValues[0], 
+           myStackValues[1], myStackValues[2]);
+
     stackDump(stk);
 
     stackDestructor(stk);
